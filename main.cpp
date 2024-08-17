d3c3186b04383ec96d6380e0f18ab5b6e128088b
DJCzubi <patryk.jap@gmail.com>
2024-08-18 01:55:14 +0200

    added trajectories and trajectory drawing

--- main.cpp
+++ main.cpp
@@ -12,6 +12,16 @@ using namespace std;
 #define WINDOW_HEIGHT 600
 
 
+#define GRAVITY_CONSTANT 9.81
+
+float particle_mass = 10;
+
+struct Trajectory
+{
+    std::vector<sf::Vector2f> points;
+};
+
+
 class StaticBody{
     public:
 
@@ -30,11 +40,72 @@ class StaticBody{
         void render(sf::RenderTexture& renderTexture){
             sf::CircleShape circle(radius);
             circle.setPosition(pos);
+            circle.setOrigin(sf::Vector2f(radius, radius));
             circle.setFillColor(color);
 
             renderTexture.draw(circle);
         }
 };
+
+float getDistanceSquared(const sf::Vector2f& point1, const sf::Vector2f& point2) {
+    float dx = point2.x - point1.x;
+    float dy = point2.y - point1.y;
+    return dx * dx + dy * dy;
+}
+
+float getDistance(const sf::Vector2f& point1, const sf::Vector2f& point2) {
+    return sqrt(getDistanceSquared(point1, point2));
+}
+
+sf::Vector2f gravityForce(StaticBody body, sf::Vector2f pos){
+    float distanceSquared = getDistanceSquared(body.pos, pos);
+
+    sf::Vector2f direction = (body.pos - pos)/sqrt(distanceSquared);
+    float magnitude = GRAVITY_CONSTANT * body.mass / distanceSquared;
+
+    return direction * magnitude;
+}
+
+sf::Vector2f netGravityForce(vector<StaticBody> bodies, sf::Vector2f pos){
+    sf::Vector2f force = {0, 0};
+
+    for(const auto& body:bodies){
+        force += gravityForce(body, pos);
+    }
+
+    return force;
+}
+
+bool colidesWithAny(vector<StaticBody> bodies, sf::Vector2f pos){
+    for(const auto& body: bodies){
+        if (getDistanceSquared(body.pos, pos) <= body.radius * body.radius)
+        return true;
+    }
+    return false;
+}
+
+Trajectory generateTrajectory(vector<StaticBody> bodies, sf::Vector2f start_pos, int maxSize = 10000, bool detectCollisions = true){
+    Trajectory traj;
+
+    traj.points.push_back(start_pos);
+
+    sf::Vector2f pos = start_pos;
+    sf::Vector2f vel = {0,0};
+    for (int step = 0; step < maxSize; step++){
+
+        if (detectCollisions){
+            if(colidesWithAny(bodies, pos))
+            return traj;
+        }
+
+        vel += netGravityForce(bodies, pos)/particle_mass;
+        pos += vel;
+
+        traj.points.push_back(pos);
+    }
+
+    return traj;
+}
 
 sf::Texture createBodiesTexture(vector<StaticBody> bodies){
 
@@ -54,18 +125,22 @@ sf::Texture createBodiesTexture(vector<StaticBody> bodies){
     return renderTexture.getTexture();
 }
 
-sf::Texture createTrajectortTexture(vector<StaticBody> bodies, array<float, 2> starting_pos){
+sf::Texture createTrajectortTexture(Trajectory trajectory, int pointRadius = 1){
     // Create a RenderTexture to draw trajectory points
     sf::RenderTexture renderTexture;
     renderTexture.create(WINDOW_WIDTH, WINDOW_HEIGHT); // Use the same size as the image
     renderTexture.clear(sf::Color::Transparent); 
 
+    for(const auto& point: trajectory.points)
+    {
+        sf::CircleShape circle(pointRadius);
+        circle.setPosition(point);
+        circle.setOrigin(sf::Vector2f(pointRadius, pointRadius));
+        circle.setFillColor(sf::Color::Red);
 
-    sf::CircleShape circle(3);
-    circle.setPosition({100,100});
-    circle.setFillColor(sf::Color::Red);
+        renderTexture.draw(circle);
+    }
 
-    renderTexture.draw(circle);
     renderTexture.display();
 
     return renderTexture.getTexture();
@@ -74,16 +149,16 @@ sf::Texture createTrajectortTexture(vector<StaticBody> bodies, array<float, 2> s
 
 int main() {
 
-    vector<StaticBody> static_bodies = {StaticBody({0, 15}, 100, sf::Color::White),
+    vector<StaticBody> static_bodies = {StaticBody({400, 105}, 100, sf::Color::White),
                                         StaticBody({150, 300}, 100, sf::Color(150, 12, 21)),
-                                        StaticBody({200, 215}, 100, sf::Color::White)};
+                                        StaticBody({600, 415}, 100, sf::Color::White)};
 
 
 
 
     //-----------   Creating test trajectory sprite    -----------
-
-    sf::Texture trajectoryTexture = createTrajectortTexture(static_bodies, {100,100});  
+    Trajectory testTrajectory = generateTrajectory(static_bodies, {100,100});
+    sf::Texture trajectoryTexture = createTrajectortTexture(testTrajectory);  
 
     // Create a sprite to display the image
     sf::Sprite trajectorySprite(trajectoryTexture);
@@ -99,6 +174,9 @@ int main() {
     // Create a window
     sf::RenderWindow window(sf::VideoMode(800, 600), "Live Rendering");
 
+
+    int traj_steps = 1;
+
     // Simulate a render process
     while (window.isOpen()) {
         sf::Event event;
@@ -110,6 +188,14 @@ int main() {
         }
 
         window.clear();
+
+
+        Trajectory testTrajectory = generateTrajectory(static_bodies, {200,100}, traj_steps);
+        sf::Texture trajectoryTexture = createTrajectortTexture(testTrajectory);  
+
+        // Create a sprite to display the image
+        sf::Sprite trajectorySprite(trajectoryTexture);
+
         
         // Draw the image sprite
         window.draw(trajectorySprite);
@@ -118,6 +204,8 @@ int main() {
         window.draw(bodiesSprite);
         
         window.display();
+
+        traj_steps ++;
 
     }
 
